a157ca79bce5466771120d8f77825b70773fa790
Gray-Falcon <126153342+Gray-Falcon@users.noreply.github.com>
2025-07-10 13:13:14 +0200

    Add files via upload

--- globalVariables.js
+++ globalVariables.js
@@ -1,4 +1,6 @@
 globalThis.myEmail = '';
+globalThis.myPassword = '';
 globalThis.domain = 'https://skylands.uk';
-// http://localhost:8080/sendVerificationEmail
-// https://skylands.uk/sendVerificationEmail
+// http://localhost:8080/verificationEmail
+// https://skylands.uk/verificationEmail
+globalThis.skylands = '';
