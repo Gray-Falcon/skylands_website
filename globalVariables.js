69f75fb68618ee20a7d9b183a2331474aa5a6bac
Gray-Falcon <126153342+Gray-Falcon@users.noreply.github.com>
2025-07-27 15:55:26 +0200

    Add files via upload
    update

--- globalVariables.js
+++ globalVariables.js
@@ -1,6 +1,42 @@
-globalThis.myEmail = '';
-globalThis.myPassword = '';
+globalThis.myEmail = undefined;
+globalThis.myPassword = undefined;
 globalThis.domain = 'https://skylands.uk';
-// http://localhost:8080/verificationEmail
-// https://skylands.uk/verificationEmail
-globalThis.skylands = '';
+// http://localhost:8080
+// https://skylands.uk
+globalThis.skylands = undefined;
+
+
+
+
+
+
+// Setup BroadcastChannel
+const channel = new BroadcastChannel('AuthChannel');
+
+// Request auth data if missing
+const loop = setInterval(() => {
+    if (!globalThis.myEmail || !globalThis.myPassword) {
+        channel.postMessage({ type: 'requestAuthData' });
+    }
+    if (globalThis.myEmail && globalThis.myPassword) clearInterval(loop);
+}, 1000);
+
+// Handle messages
+channel.onmessage = (e) => {
+    const { type, myEmail, myPassword } = e.data;
+
+    // Receive auth data
+    if (type === 'authData') {
+        globalThis.myEmail = myEmail;
+        globalThis.myPassword = myPassword;
+    }
+
+    // Respond to auth data requests
+    if (type === 'requestAuthData' && globalThis.myEmail && globalThis.myPassword) {
+        channel.postMessage({
+            type: 'authData',
+            myEmail: globalThis.myEmail,
+            myPassword: globalThis.myPassword
+        });
+    }
+};
